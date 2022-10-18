3c9d9446ec555a52b139a0be095cd4f68ab2f120
JanluOfficial <95766563+JanluOfficial@users.noreply.github.com>
2022-10-19 01:01:49 +0200

    Update startup.py

--- startup.py
+++ startup.py
@@ -2,3 +2,8 @@ import json
 import os
 import platform
 import framework.getConsoleSize as getConSize
+
+class Data_Collector:
+    infofile = open("info.json", "r").read()
+    infoJSON = json.loads(infofile)
+    
