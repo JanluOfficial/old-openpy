77dc489cb1ae0f72673d092ac82b05ce0639bd7b
JanluOfficial <95766563+JanluOfficial@users.noreply.github.com>
2022-11-08 19:16:41 +0100

    Renamed OOBE App

---
+++ apps/OutOfBoxExperience/OOBE.py
@@ -0,0 +1,25 @@
+from rich.console import Console
+from rich.prompt import Prompt
+from rich.console import Group
+from rich.panel import Panel
+
+opstr = "[bold][yellow]Open[/yellow][blue]Py[/blue][/bold]"
+console = Console()
+class screens:
+    def WelcomeScreen():
+        console.print(Panel(f"Welcome to {opstr}!", title_align='center'))
+        WelcomeBox = Panel("This App will help you to set up OpenPy. If this\nis your first time using OpenPy, please read the instructions [red]CAREFULLY![/red]")
+        console.print(WelcomeBox)
+        Prompt.ask('Press Enter to Continue')
+
+    def EULAScreen():
+        EULAgroup = Group(
+            Panel('Please read the EULA (End User License Agreement) and DLA (Developer License Agreement) before accepting them.\n\n', title='EULA'),
+        )
+        console.print(EULAgroup)
+        console.print('If you do not want to agree to the EULA and DLA, please close OpenPy Imediately!')
+        Prompt.ask('Press Enter to Agree to the EULA and DLA')
+
+class Setup:
+    screens.WelcomeScreen()
+    screens.EULAScreen()
--- apps/OutOfBoxExperience/screens/screens.py
+++ apps/OutOfBoxExperience/screens/screens.py
@@ -3,17 +3,21 @@ from rich.prompt import Prompt
 from rich.console import Group
 from rich.panel import Panel
 
+
+
 opstr = "[bold][yellow]Open[/yellow][blue]Py[/blue][/bold]"
 console = Console()
 def WelcomeScreen():
     console.print(Panel(f"Welcome to {opstr}!", title_align='center'))
     WelcomeBox = Panel("This App will help you to set up OpenPy. If this\nis your first time using OpenPy, please read the instructions [red]CAREFULLY![/red]")
     console.print(WelcomeBox)
-    print()
     Prompt.ask('Press Enter to Continue')
 
 def EULAScreen():
     EULAgroup = Group(
-        Panel('Please read the EULA (End User License Agreement) and DLA (Developer License Agreement) before accepting them.\n\n', title='EULA')
+        Panel('Please read the EULA (End User License Agreement) and DLA (Developer License Agreement) before accepting them.\n\n', title='EULA'),
     )
     console.print(EULAgroup)
+    console.print('If you do not want to agree to the EULA and DLA, please close OpenPy Imediately!')
+    Prompt.ask('Press Enter to Agree to the EULA and DLA')
+
