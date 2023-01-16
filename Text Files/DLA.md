4852ae7413f1e24f662e99b4d7a666e99c1656e3
JanluOfficial <95766563+JanluOfficial@users.noreply.github.com>
2023-01-16 19:03:04 +0100

    removed everything for a fresh start

--- Text Files/DLA.md
+++
@@ -1,3 +0,0 @@
-# The DLA is coming soon
-
-All you need to know is that you can't make anything that has to do with 18+ Content.
--- framework/OOBEapply.py
+++
@@ -1,5 +0,0 @@
-import os
-
-def apply(username: str):
-    open(os.getenv('APPDATA') + '\\OpenPy\\user\\settings\\username.ops', 'w').write(username)
-    open(os.getenv('APPDATA') + '\\OpenPy\\user\\settings\\oobeHasRan.ops', 'w').write('true')
