0fe81b7df476777aabfab2c7e58c7ab1ea7be576
JanluOfficial <95766563+JanluOfficial@users.noreply.github.com>
2023-01-31 23:17:11 +0100

    Update terminal.py

--- framework/terminal.py
+++ framework/terminal.py
@@ -12,21 +12,26 @@ def terminal():
 
     if cmd.startswith("cloudrun "):
         with Progress() as progress:
-            task1 = progress.add_task("[purple4]▄[grey100 on purple4] Start [/grey100 on purple4]▀[/purple4]", total=6)
+            try:
+                task1 = progress.add_task("[purple4]▄[grey100 on purple4] Start [/grey100 on purple4]▀[/purple4]", total=6)
 
-            lists = requests.get("https://raw.githubusercontent.com/JanluOfficial/opr-library/master/main.json")
-            progress.update(task1, advance=1)
-            listjson = json.loads(lists.content)
-            progress.update(task1, advance=1)
-            applist = requests.get(listjson["applisturl"])
-            progress.update(task1, advance=1)
-            applistjson = json.loads(applist.content)
-            progress.update(task1, advance=1)
-            app_info = json.loads(requests.get(applistjson["cloudrun"][re.sub(r'^cloudrun ', '', cmd)]).content)
-            progress.update(task1, advance=1)
-            app_code = requests.get(app_info["file"]).content
-            progress.update(task1, advance=1)
-        exec(app_code)
+                lists = requests.get("https://raw.githubusercontent.com/JanluOfficial/opr-library/master/main.json")
+                progress.update(task1, advance=1)
+                listjson = json.loads(lists.content)
+                progress.update(task1, advance=1)
+                applist = requests.get(listjson["applisturl"])
+                progress.update(task1, advance=1)
+                applistjson = json.loads(applist.content)
+                progress.update(task1, advance=1)
+                app_info = json.loads(requests.get(applistjson["cloudrun"][re.sub(r'^cloudrun ', '', cmd)]).content)
+                progress.update(task1, advance=1)
+                app_code = requests.get(app_info["file"]).content
+                progress.update(task1, advance=1)
+            except KeyError:
+                print("[red3]Error![/red3] This cloudrun app might not exist.")
+        try:
+            exec(app_code)
+        except UnboundLocalError: otk = 0
 
     elif cmd.startswith("cloudsearch "):
         with Progress() as progress:
@@ -41,8 +46,10 @@ def terminal():
             progress.update(task1, advance=1)
             app_list_json = json.loads(app_list.content)
             progress.update(task2, advance=1)
+
             searchterm = re.sub(r'^cloudsearch ', '', cmd)
             tree = Tree("[dark_cyan]▄[grey100 on dark_cyan] Results [/grey100 on dark_cyan]▀[/dark_cyan]")
+
             for app in app_list_json["cloudrun"]["all-cloudrun-apps-list"]:
                 app_data = json.loads(requests.get(app_list_json["cloudrun"][app]).content)
                 if searchterm in app_data["name"] or searchterm in app:
@@ -53,4 +60,28 @@ def terminal():
                 progress.update(task2, advance=(1000-2)/len(app_list_json["cloudrun"]["all-cloudrun-apps-list"]))
             print(tree)
 
-                
+    elif cmd == "cloudlist":
+        with Progress() as progress:
+            task1 = progress.add_task("[purple4]▄[grey100 on purple4] Gathering List [/grey100 on purple4]▀[/purple4]", total=2)
+            task2 = progress.add_task("[purple4]▀[grey100 on purple4] Processing [/grey100 on purple4]▀[/purple4]", total=1000)
+
+            lists = requests.get("https://raw.githubusercontent.com/JanluOfficial/opr-library/master/main.json")
+            progress.update(task1, advance=1)
+            list_json = json.loads(lists.content)
+            progress.update(task2, advance=1)
+            app_list = requests.get(list_json["applisturl"])
+            progress.update(task1, advance=1)
+            app_list_json = json.loads(app_list.content)
+            progress.update(task2, advance=1)
+
+            searchterm = re.sub(r'^cloudsearch ', '', cmd)
+            tree = Tree("[dark_cyan]▄[grey100 on dark_cyan] Results [/grey100 on dark_cyan]▀[/dark_cyan]")
+
+            for app in app_list_json["cloudrun"]["all-cloudrun-apps-list"]:
+                app_data = json.loads(requests.get(app_list_json["cloudrun"][app]).content)
+                to_add_to = tree.add(app_data["name"].replace(searchterm, f'[green3]{searchterm}[/green3]'))
+                to_add_to.add(f'{app_data["description"]}')
+                to_add_to.add(app_data["file"])
+                to_add_to.add("Run Name").add(app.replace(searchterm, f'[green3]{searchterm}[/green3]'))
+                progress.update(task2, advance=(1000-2)/len(app_list_json["cloudrun"]["all-cloudrun-apps-list"]))
+            print(tree)
